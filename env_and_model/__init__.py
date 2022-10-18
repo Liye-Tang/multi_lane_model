d54f32501762f7c2f7a5a1a1d52ccbce8ef5cbf2
tly <tly20@mails.tsinghua.edu.cn>
2022-10-18 17:42:54 +0800

    minor change

--- env_and_model/__init__.py
+++ env_and_model/__init__.py
@@ -7,15 +7,15 @@
 # @FileName: __init__.py
 # =====================================
 
-from env_and_model.aircraft.aircraft_env import AircraftEnv, AircraftModel
-from env_and_model.idc_real.endtoend import IdcRealEnv
-from env_and_model.idc_real.dynamics_and_models import IdcRealModel
-from env_and_model.idc_virtual.endtoend import IdcVirtualEnv
-from env_and_model.idc_virtual.dynamics_and_models import IdcVirtualModel
-from env_and_model.idc_virtual.e2e_planner.end2end import E2eEnv
-from env_and_model.idc_virtual_veh.endtoend import IdcVirtualVehEnv
-from env_and_model.idc_virtual_veh.dynamics_and_models import IdcVirtualVehModel
-from env_and_model.path_tracking.path_tracking_env import PathTrackingEnv, PathTrackingModel
+# from env_and_model.aircraft.aircraft_env import AircraftEnv, AircraftModel
+# from env_and_model.idc_real.endtoend import IdcRealEnv
+# from env_and_model.idc_real.dynamics_and_models import IdcRealModel
+# from env_and_model.idc_virtual.endtoend import IdcVirtualEnv
+# from env_and_model.idc_virtual.dynamics_and_models import IdcVirtualModel
+# from env_and_model.idc_virtual.e2e_planner.end2end import E2eEnv
+# from env_and_model.idc_virtual_veh.endtoend import IdcVirtualVehEnv
+# from env_and_model.idc_virtual_veh.dynamics_and_models import IdcVirtualVehModel
+# from env_and_model.path_tracking.path_tracking_env import PathTrackingEnv, PathTrackingModel
 from env_and_model.multi_lane.multilane import MultiLane
 from env_and_model.multi_lane.car_tracking.dynamics_and_models import MultiLaneModel
 
