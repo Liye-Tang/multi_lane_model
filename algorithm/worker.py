5e3b7aa28b586bc5ec1b5573dd43a354321c1abc
tly <tly20@mails.tsinghua.edu.cn>
2022-10-20 17:40:20 +0800

    test worker

--- algorithm/worker.py
+++ algorithm/worker.py
@@ -180,21 +180,55 @@ class OffPolicyWorker(object):
         # self.policy_with_value.apply_gradients(self.tf.constant(iteration, dtype=self.tf.int32), grads)
         self.policy_with_value.apply_gradients(iteration, grads)
 
+    # def sample(self):
+    #     batch_data = []
+    #     reward_dict_list = []
+    #     for _ in range(int(self.sample_batch_size / self.num_agent)):
+    #         reward_dict_list.append({})
+    #         for i in range(self.num_agent):
+    #             batch_data.append(
+    #                 (self.obs.copy(), 0, 0, 0, 0, 0)
+    #             )
+    #         self.obs = self.env.reset()
+    #
+    #     if self.worker_id == 1 and self.sample_times % self.args.worker_log_interval == 0:
+    #         logger.info('Worker_info: {}'.format(self.get_stats()))
+    #     # for k, _ in reward_dict_list[0].items():
+    #     #     self.stats[k+'_mean_data'] = self.tf.reduce_mean([reward_dict[k] for reward_dict in reward_dict_list])
+    #     self.num_sample += len(batch_data)
+    #     self.sample_times += 1
+    #     return batch_data
+
     def sample(self):
         batch_data = []
         reward_dict_list = []
         for _ in range(int(self.sample_batch_size / self.num_agent)):
-            reward_dict_list.append({})
-            for i in range(self.num_agent):
-                batch_data.append(
-                    (self.obs.copy(), 0, 0, 0, 0, 0)
-                )
+            processed_obs = self.preprocessor.process_obs(self.obs)
+            judge_is_nan([processed_obs])
+            action, _ = self.policy_with_value.compute_action(self.tf.constant(processed_obs[np.newaxis, :]))
+            if self.explore_sigma is not None:
+                action += np.random.normal(0, self.explore_sigma, np.shape(action))
+            # try:
+            #     judge_is_nan([action])
+            # except ValueError:
+            #     print('processed_obs', processed_obs)
+            #     print('policy_weights', self.policy_with_value.policy.trainable_weights)
+            #     action, logp = self.policy_with_value.compute_action(processed_obs)
+            #     judge_is_nan([action])
+            #     raise ValueError
+            obs_tp1, reward, done, info = self.env.step(action[0].numpy())
+            # reward_dict_list.append(info['reward_dict'])
+            punish = info['punish']
+            batch_data.append(
+                (self.obs.copy(), action.numpy(), reward,
+                 obs_tp1.copy(), done, punish))
+            # for multiagent env, we need to reset it every time
             self.obs = self.env.reset()
 
         if self.worker_id == 1 and self.sample_times % self.args.worker_log_interval == 0:
             logger.info('Worker_info: {}'.format(self.get_stats()))
         # for k, _ in reward_dict_list[0].items():
-        #     self.stats[k+'_mean_data'] = self.tf.reduce_mean([reward_dict[k] for reward_dict in reward_dict_list])
+        #     self.stats[k + '_mean_data'] = self.tf.reduce_mean([reward_dict[k] for reward_dict in reward_dict_list])
         self.num_sample += len(batch_data)
         self.sample_times += 1
         return batch_data
